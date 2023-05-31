b82ef1d66ca83be15ad92670ca48b90770f3df7b
mickiyas123 <mikikinfe345@gmail.com>
2023-05-31 06:44:59 +0300

    fix: detect sfari and show message

--- src/App.tsx
+++ src/App.tsx
@@ -32,17 +32,17 @@ import { useAtom } from 'jotai';
 import { loadingAtom } from './store/store';
 import loadingGif from '@assets/images/loading.gif';
 import HandContextProvider from './context/HandContext';
-// import UsePC from './pages/UsePC';
+import UsePC from './pages/UsePc';
 
 function App() {
   const [loading] = useAtom(loadingAtom);
 
-  // const userAgent = navigator.userAgent;
-  // const isSafari = userAgent.includes('Safari');
+  const userAgent = navigator.userAgent;
+  const isSafari = userAgent.includes('Safari');
 
-  // const isMobile = /Android|iPhone/i.test(navigator.userAgent);
-  // if (isMobile) return <UsePC message="mobile devices" />;
-  // if (isSafari) return <UsePC message="safari browsers" />
+  const isMobile = /Android|iPhone/i.test(navigator.userAgent);
+  if (isMobile) return <UsePC message="mobile devices" />;
+  if (isSafari) return <UsePC message="safari browsers" />
 
   if (loading)
     return (
